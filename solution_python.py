0edcdbd27d93c4228f875a9f50bd449cf7b7cc73
Harveyjustin087 <60191338+Harveyjustin087@users.noreply.github.com>
2020-07-10 14:56:49 -0400

    Update solution_python.py

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,55 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.history = 0
+        self.change = []
 
     def add(self, num: int):
-        pass
+        final = 0
+        final = num + self.value
+        self.history += 1
+        self.change.append(final)
+        return final
 
     def subtract(self, num: int):
-        pass
+        final = 0
+        final = self.value - num
+        self.history += 1
+        self.change.append(final)
+        return final
 
     def undo(self):
-        pass
+        final = 0
+        if(self.history > 0):
+            self.history -= 1
+            final = int(self.change[self.history])
+        else:
+            print("Nothing to Undo")
+        return final
 
     def redo(self):
-        pass
+        final = 0
+        if(self.history + 1 < len(self.change)):
+            self.history += 1
+            final = int(self.change[self.history])
+        else:
+            print("Nothing to Redo")
+        return final
 
     def bulk_undo(self, steps: int):
-        pass
+        final = 0
+        if(steps > self.change[steps] ):
+            print("index out of range")
+        else:
+            final = int(self.change[steps])
+            self.history -= steps
+        return final
 
     def bulk_redo(self, steps: int):
-        pass
+        final = 0
+        if(steps > self.change[steps] ):
+            print("index out of range")
+        else:
+            final = int(self.change[steps])
+            self.history += steps
+        return final
